c6f38297587a8af24773d656092483345f331c68
Shivani <shivani.bhaskar@nxtgen.com>
2025-07-29 14:25:44 +0530

    oneplus array progarm

--- Practice/Practice/dsa/Array.js
+++ Practice/Practice/dsa/Array.js
@@ -20,5 +20,21 @@ const Array = () => {
   const spliceArr = newArr.splice(2, 1);
   //   console.log({ spliceArr });
   //   console.log({ newArr });
+
+  function onPlus(digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+        digits[i]++;
+        return digits;
+      }
+      digits[i] = 0;
+    }
+
+    digits.unshift(1);
+    return digits;
+  }
+  const digits = [1, 2, 3];
+
+  console.log(onPlus([1, 2, 3]));
 };
 export default Array;
